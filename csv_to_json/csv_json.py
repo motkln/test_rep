5538cab9471ce47d917484b02a178ad1fa72e78b
Матвей Калинин <pilezam@mail.ru>
2025-04-14 21:08:43 +0300

    export csv to json string

--- csv_to_json/csv_json.py
+++ csv_to_json/csv_json.py
@@ -1,6 +1,5 @@
 import json
 import csv
-from textwrap import indent
 
 person = {
     "Имя": "Матвей",
@@ -17,13 +16,11 @@ def read_csv(file, delimiter=',', lineterminator='\n'):
     return data
 
 
-def add_to_json(file, add_data):
-    print(add_data)
-    with open(file, 'w', encoding='utf-8') as json_file:
-        json.dump(add_data, json_file, ensure_ascii=False, indent=4)
-    return
+def add_to_json_str(add_data):
+    json_str = json.dumps(add_data, ensure_ascii=False, indent=4)
+    return json_str
 
 
 if __name__ == '__main__':
     data_list = read_csv('data.csv')
-    add_to_json('data.json', data_list)
+    print(add_to_json_str(data_list))
